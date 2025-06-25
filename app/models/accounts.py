c12f70f742e7c4d8c4d6c233987bc96c2cf112f8
Eddielyn Joy Bautista <eddielynjoybautista0209@gmail.com>
2025-06-25 14:40:32 +0800

    Login Backend Update

---
+++ app/models/accounts.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Account(db.Model):
+    __tablename__ = 'accounts'
+    
+    account_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    account_password = db.Column(db.String(255), nullable=False)
+    access_level = db.Column(db.Enum('owner', 'staff'), default='staff', nullable=False)
+    account_status = db.Column(db.Enum('active', 'inactive'), default='active', nullable=False)
