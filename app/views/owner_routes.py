632dea291cbcc3af2acad16f22dc00cd29db26b4
Marielle Modesto <modestomarielle127@gmail.com>
2025-06-26 13:25:29 +0800

    owner dashboard 50.30%

--- app/views/owner_routes.py
+++ app/views/owner_routes.py
@@ -20,15 +20,31 @@ def appointments():
 
 @owner.route('/appointment_req')
 def appointment_req():
-    return render_template('/owner/appointment_req.html')
+    return render_template('/owner/appointment_request.html')
 
 @owner.route('/patients')   
 def patients():
     return render_template('/owner/patients.html')
 
+@owner.route('/patient_info')   
+def patient_info():
+    return render_template('/owner/patient_info.html')
+
+@owner.route('/patient_procedure')   
+def patient_procedure():
+    return render_template('/owner/procedure.html')
+
+@owner.route('/patient_dental_rec')   
+def patient_dental_rec():
+    return render_template('/owner/dental_record.html')
+
 @owner.route('/employees')
 def employees():
     return render_template('/owner/employees.html')
+
+@owner.route('/employee_info')
+def employee_info():
+    return render_template('/owner/employee_basic_info.html')
 
 @owner.route('/inventory')
 def inventory():
