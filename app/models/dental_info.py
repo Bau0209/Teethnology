7ee7cc31b46d64371caf970723ec7e435a008693
Eddielyn Joy Bautista <eddielynjoybautista0209@gmail.com>
2025-07-01 23:11:03 +0800

    update

--- app/models/dental_info.py
+++ app/models/dental_info.py
@@ -1,17 +1,15 @@
 from app import db
-from datetime import date
 
-class PatientDentalInfo(db.Model):
+class DentalInfo(db.Model):
     __tablename__ = 'dental_record'
 
     dental_record_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    patient_id = db.Column(db.Integer, db.ForeignKey('patients.patient_id'), nullable=False)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient_info.patient_id'), nullable=False)
     dental_record_image_link = db.Column(db.Text, nullable=False)
     periodontal_screening = db.Column(db.Text)
     occlusion = db.Column(db.Text)
     appliances = db.Column(db.Text)
     TMD = db.Column(db.Text)
-    date_taken = db.Column(db.Date, default=date.today)
 
     # Optional: define relationship to Patient model if needed
-    patient = db.relationship('Patient', backref='dental_records')
+    patient = db.relationship('PatientsInfo', backref=db.backref('dental_record', lazy=True, cascade="all, delete-orphan"))
