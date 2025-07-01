1932015185d439163c8e3ff5189b4d0a55f9d649
Eddielyn Joy Bautista <eddielynjoybautista0209@gmail.com>
2025-07-01 19:16:53 +0800

    Added the new database file

---
+++ app/models/__init__.py
@@ -0,0 +1,9 @@
+from .accounts import Account
+from .appointments import Appointments
+from .branches import Branch, ClinicBranchImage
+from .employees import Employee
+from .patient_medical_info import PatientMedicalInfo
+from .patients import PatientsInfo
+from .procedures import Procedures
+from .transactions import Transactions
+from .dental_info import DentalInfo
