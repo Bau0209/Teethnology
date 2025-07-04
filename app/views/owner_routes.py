b1e764b865eeefba5c066829fea3eaa618ff83d2
Eddielyn Joy Bautista <eddielynjoybautista0209@gmail.com>
2025-07-04 17:28:09 +0800

    Beautify the owner dashboard

--- app/views/owner_routes.py
+++ app/views/owner_routes.py
@@ -16,7 +16,6 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-from flask import request
 @owner.route('/owner_home')
 def owner_home():
     today = date.today()
