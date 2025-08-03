7799b5af75b23933e55ef7ed3f09044de7229edc
Marielle Modesto <modestomarielle127@gmail.com>
2025-08-03 20:35:10 +0800

    done but not connected to database

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,10 @@
 from .accounts import Account
-from .appointments import Appointments, ArchivedAppointments
+from .archive import Archive
+from .appointments import Appointments
 from .branches import Branch, ClinicBranchImage
 from .employees import Employee
 from .patient_medical_info import PatientMedicalInfo
-from .patients import PatientsInfo, ArchivedPatientsInfo
+from .patients import PatientsInfo
 from .procedures import Procedures
 from .transactions import Transactions
 from .dental_info import DentalInfo
